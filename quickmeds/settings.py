79102baa190dd1d8edf75ee86d8b403f22a9df21
Rhushabh Gumte <rhushabhgumte0485@gmail.com>
2025-04-17 01:06:26 +0530

    Fixed database configuration

--- quickmeds/settings.py
+++ quickmeds/settings.py
@@ -92,14 +92,19 @@ WSGI_APPLICATION = 'quickmeds.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# If DATABASE_URL is set, use it
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 
 
 # Password validation
